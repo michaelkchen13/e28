55371207dbb907986fcb40d5eb7da480856b1704
Michael Chen <michaelkchen13@gmail.com>
2020-09-24 23:14:34 -0400

    add functionality and build RPS game

--- p1/script.js
+++ p1/script.js
@@ -1,6 +1,71 @@
 let app = new Vue({
     el: '#app',
     data: {
-        name: "Mike",
+        playerMove: "",
+        compMove: "",
+        playerWin: 0,
+        compWin: 0,
+        roundNum: 1,
+        winList: [],
+        winner: ""
+    },
+    methods: {
+        playGame() {
+            moves = ["rock", "paper", "scissors"];
+
+            computersMoveNum = Math.floor(Math.random() * 3);
+            this.compMove = moves[computersMoveNum];
+
+            obj = {};
+
+            // Player Chooses Rock
+            if (this.playerMove == "rock") {
+                if (this.compMove == "rock") {
+                    this.winner = "Noone";
+                }
+                if (this.compMove == "paper") {
+                    this.winner = "Computer";
+                    this.compWin++;
+                }
+                if (this.compMove == "scissors") {
+                    this.winner = "Player";
+                    this.playerWin++;
+                }
+            }
+            // Player Chooses Paper
+            if (this.playerMove == "paper") {
+                if (this.compMove == "rock") {
+                    this.winner = "Player";
+                    this.playerWin++;
+                }
+                if (this.compMove == "paper") {
+                    this.winner = "Noone";
+                }
+                if (this.compMove == "scissors") {
+                    this.winner = "Computer";
+                    this.compWin++;
+                }
+            }
+            // Player Chooses Scissors
+            if (this.playerMove == "scissors") {
+                if (this.compMove == "rock") {
+                    this.winner = "Computer";
+                    this.compWin++;
+                }
+                if (this.compMove == "paper") {
+                    this.winner = "Player";
+                    this.playerWin++;
+                }
+                if (this.compMove == "scissors") {
+                    this.winner = "Noone";
+                }
+            }
+
+            obj["round"] = this.roundNum;
+            obj["winner"] = this.winner;
+            this.winList.push(obj);
+
+            this.roundNum++;
+        }
     }
 })
